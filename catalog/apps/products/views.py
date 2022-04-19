33a55eb8c451c54518f805af4c266a1a636a4de5
thony19cr <acarrillorj@gmail.com>
2022-04-19 12:52:37 -0500

    feat: Product GET endpoint Allow Any

--- catalog/apps/products/views.py
+++ catalog/apps/products/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -6,3 +7,8 @@ from .serializers import ProductSerializer
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [AllowAny()]
+        return super(ProductViewSet, self).get_permissions()
